44d1a3294967020015f3057ef0185625a1f95f36
ApolloSolo <matt1teixeira@gmail.com>
2022-01-31 19:05:39 -0500

    started function for randomPassword

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-const btn = document.querySelector('#btn')
+const btnFun = document.querySelector('#btn-fun')
 const displayPass = document.querySelector('#pass-display')
 
 const verbs =  [
@@ -70,6 +70,8 @@ const nouns = [
     "Rock"
 ]
 
+const symboles = ["!", "@", "#", "$", "%", "&"]
+
 const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 const randomNum = (limit) => {
@@ -77,19 +79,56 @@ const randomNum = (limit) => {
     return num;
 }
 
-const createPassword = (verb, noun, num) => {
-    const randVerb = randomNum(verbs.length-1)
-    const randomNoun = randomNum(nouns.length-1)
-    const randNum1 = randomNum(nums.length-1)
-    const randNum2 = randomNum(nums.length-1)
-    const randNum3 = randomNum(nums.length-1)
+//Takes in an array, pick random index, and creates new array of mixed up letters
+const randomizeArrayOfArrays = (oldArray) => {
+    const newArray = []
+    const randArrayIndex = oldArray[randomNum(oldArray.length-1)];
+    for(let i = 0; i < randArrayIndex.length - 1; i++){
+        newArray.push(randArrayIndex[randomNum(randArrayIndex.length-1)])
+    }
+    return newArray;
+}
+
+const randomizeArray = (oldArray) => {
+    const newArray = []
+    for(let i = 0; i < oldArray.length - 1; i++){
+        newArray.push(oldArray[randomNum(oldArray.length-1)])
+    }
+    return newArray;
+}
+
+const createFunPassword = (verb, noun, num) => {
+    const randVerb = randomNum(verb.length-1)
+    const randomNoun = randomNum(noun.length-1)
+    const randNum1 = randomNum(num.length-1)
+    const randNum2 = randomNum(num.length-1)
+    const randNum3 = randomNum(num.length-1)
     const password = `${verb[randVerb]}${noun[randomNoun]}${num[randNum1]}${num[randNum2]}${num[randNum3]}`
 
     return password;
 }
 
 
-btn.addEventListener('click', (e) => {
-    const myPassword = createPassword(verbs, nouns, nums);
+const createRandomPassword = (verb, numOfNums, symbole) => {
+    const randomLetters = randomizeArrayOfArrays(verb);
+    const randomNums = [];
+    const randSymbole = symbole[randomNum(symbole.length-1)]
+    for(let i = 0; i < numOfNums; i++){
+        let num = randomNum(numOfNums);
+        randomNums.push(num);
+    }
+    
+    const combo = randomLetters.concat(randomNums);
+    combo.push(randSymbole)
+    const randCombo = randomizeArray(combo);
+    return randCombo;
+}
+
+let letters = createRandomPassword(verbs, 4, symboles);
+
+
+
+btnFun.addEventListener('click', (e) => {
+    const myPassword = createFunPassword(verbs, nouns, nums);
     displayPass.value = myPassword;
 })
